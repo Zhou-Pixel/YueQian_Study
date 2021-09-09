8fb4cdf1e8acecb2d55440c8fb29d656e6d0f711
ZhouGuiqing <1304806004@qq.com>
2021-09-09 17:29:55 +0800

    测试

--- code/Daily/9.6/test_strlen.c
+++ code/Daily/9.6/test_strlen.c
@@ -1,14 +1,13 @@
 /*
  * @Author: ZhouGuiqing
  * @Date: 2021-09-06 15:20:37
- * @LastEditTime: 2021-09-06 15:21:45
+ * @LastEditTime: 2021-09-09 17:29:47
  * @LastEditors: ZhouGuiqing
  * @Description: 
- * @FilePath: /code/Daily/9.6/test_strlen.c
+ * @FilePath: /YueQian/code/Daily/9.6/test_strlen.c
  */
 #include <string.h>
 #include <stdio.h>
-
 
 int main(int argc, char const *argv[])
 {
