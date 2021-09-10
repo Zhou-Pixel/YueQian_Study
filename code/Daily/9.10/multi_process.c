d6d397e24b519c5040611595f91cff09a80c5dd4
ZhouGuiqing <1304806004@qq.com>
2021-09-10 18:50:29 +0800

    错误过滤修复

---
+++ code/Daily/9.10/multi_process.c
@@ -0,0 +1,8 @@
+/*
+ * @Author: ZhouGuiqing
+ * @Date: 2021-09-10 18:46:20
+ * @LastEditTime: 2021-09-10 18:48:32
+ * @LastEditors: ZhouGuiqing
+ * @Description: 多进程作业
+ * @FilePath: /YueQian/code/Daily/9.10/multi_process.c
+ */
