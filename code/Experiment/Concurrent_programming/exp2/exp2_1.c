1f16ae229ddfe8c1049845fd81ab09a05c696272
ZhouGuiqing <1304806004@qq.com>
2021-09-14 09:40:42 +0800

    test

--- code/Experiment/Concurrent_programming/exp2/exp2_1.c
+++ code/Experiment/Concurrent_programming/exp2/exp2_1.c
@@ -1,10 +1,10 @@
 /*
  * @Author: ZhouGuiqing
  * @Date: 2021-09-13 18:54:19
- * @LastEditTime: 2021-09-13 18:54:19
+ * @LastEditTime: 2021-09-14 09:39:19
  * @LastEditors: ZhouGuiqing
  * @Description: 
- * @FilePath: /YueQian/code/Experiment/Concurrent_programming/exp2/exp1_1.c
+ * @FilePath: /YueQian/code/Experiment/Concurrent_programming/exp2/exp2_1.c
  */
 #include <stdio.h>
 #include <stdlib.h>
@@ -12,6 +12,7 @@
 #include <syslog.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <string.h>
 
 int main(int argc, char const *argv[])
 {
@@ -25,9 +26,13 @@ int main(int argc, char const *argv[])
     {
         dup2(pipe_fd[1], STDOUT_FILENO);
         execlp("ls", "ls", "/etc", NULL);
+        printf("测试\n");
         exit(0);
     }
 
+    // char buf[100] = {0};
+    // sprintf(buf, "test");
+    // write(pipe_fd[1], buf, strlen(buf));
     close(pipe_fd[1]);
     dup2(pipe_fd[0], STDIN_FILENO);
 
