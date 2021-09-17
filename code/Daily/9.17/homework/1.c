3f3dc5c274c8c2a9ca9575b0e52b41cef26c79b9
ZhouGuiqing <1304806004@qq.com>
2021-09-17 20:30:55 +0800

    0917作业

---
+++ code/Daily/9.17/homework/1.c
@@ -0,0 +1,59 @@
+/*
+ * @Author: ZhouGuiqing
+ * @Date: 2021-09-17 18:59:50
+ * @LastEditTime: 2021-09-17 19:41:27
+ * @LastEditors: ZhouGuiqing
+ * @Description: 1，利用线程间的信号量实现一个逻辑
+ *	从键盘中获取数据，用另外一条线程将数据打印出来
+ *		如果没有打印数据出来则不能再次往里面输入字符，如果没有输入字符则不能将数据打印出来
+ * @FilePath: /YueQian/code/Daily/9.17/homework/1.c
+ */
+
+
+#include <stdio.h>
+#include <semaphore.h>
+#include <pthread.h>
+#include <unistd.h>
+
+
+sem_t sem_read;
+sem_t sem_write;
+char buf[100];
+
+void *thread_printf(void *arg)
+{
+    while (1)
+    {
+        sem_wait(&sem_read);
+        printf("打印线程打印中：%s\n", buf);
+        sleep(1);
+        sem_post(&sem_write);
+    }
+    
+}
+
+
+
+
+
+int main(int argc, char const *argv[])
+{
+    
+    sem_init(&sem_read, 0, 0);
+    sem_init(&sem_write, 0, 1);
+
+    pthread_t tid;
+    pthread_create(&tid, NULL, thread_printf, NULL);
+
+    while (1)
+    {
+        sem_wait(&sem_write);
+        printf("请输入:");
+        fgets(buf, 100, stdin);
+        sem_post(&sem_read);
+    }
+    
+
+    return 0;
+}
+
---
+++ code/Daily/9.17/homework/2.c
@@ -0,0 +1,98 @@
+/*
+ * @Author: ZhouGuiqing
+ * @Date: 2021-09-17 19:41:29
+ * @LastEditTime: 2021-09-17 20:24:41
+ * @LastEditors: ZhouGuiqing
+ * @Description: 
+ * @FilePath: /YueQian/code/Daily/9.17/homework/2.c
+ */
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <signal.h>
+#include <string.h>
+#include <strings.h>
+
+int count = 5;
+pthread_cond_t cond;
+pthread_mutex_t lock;
+
+
+
+
+void *count_v(void *arg)
+{
+    pthread_mutex_lock(&lock);
+    while (1)
+    {
+        if (count <= 0)
+        {
+            pthread_cond_wait(&cond, &lock);
+        }
+        
+        sleep(1);
+        printf("减1\n");
+        count--;        
+    }
+
+    pthread_mutex_unlock(&lock);
+    
+}
+
+void sigint_handler(int signum)
+{
+    pthread_mutex_lock(&lock);
+
+    printf("count+1\n");
+    count += 1;
+
+    pthread_mutex_unlock(&lock);
+
+    pthread_cond_signal(&cond);
+
+}
+
+int main(int argc, char const *argv[])
+{
+
+    signal(SIGINT, sigint_handler);
+
+    pthread_t tid[3];
+    pthread_mutex_init(&lock, NULL);
+    pthread_cond_init(&cond, NULL);
+
+    for (int i = 0; i < 3; i++)
+    {
+        pthread_create(tid+i, NULL, count_v, NULL);
+    }
+    
+    char buf[100];
+
+    while (1)
+    {
+        printf("请输入：");
+        fgets(buf, 100, stdin);
+        strtok(buf, "\n");
+        if (strcmp(buf, "exit") == 0)
+        {
+            break;
+        }
+        
+    }
+    
+
+    
+
+    for (int i = 0; i < 3; i++)
+    {
+        pthread_cancel(tid[i]);
+    }
+
+
+    pthread_mutex_destroy(&lock);
+    pthread_cond_destroy(&cond);
+    
+
+
+    return 0;
+}
